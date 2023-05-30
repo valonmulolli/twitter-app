4d076be5e1317dabd87bef351f98791883bd1613
valonmulolli <83613203+valonmulolli@users.noreply.github.com>
2023-05-30 22:19:42 +0200

    Update Tweet.tsx

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet,Pressable } from 'react-native';
 import { TweetType } from '../types';
-<<<<<<< HEAD
 import { Entypo } from '@expo/vector-icons';
 import IconButton from './IconButton';
 import { Link } from 'expo-router';
-=======
-import { Entypo, EvilIcons } from '@expo/vector-icons';
-
-type IconButtonProps = {
-	icon: React.ComponentProps<typeof EvilIcons>['name'];
-	text?: string | number;
-};
-
-const IconButton = ({ icon, text }: IconButtonProps) => {
-	return (
-		<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-			<EvilIcons name={icon} size={24} color='grey' />
-			<Text style={{ fontSize: 12, color: 'gray' }}>{text}</Text>
-		</View>
-	);
-};
->>>>>>> 2b0fa301eb6f0d01f4b1df124f5880925c1ff8b0
 
 type TweetProps = {
 	tweet: TweetType;
@@ -29,7 +11,6 @@ type TweetProps = {
 
 const Tweet = ({ tweet }: TweetProps) => {
 	return (
-<<<<<<< HEAD
 		<Link href={`/tweet/${tweet.id}`} asChild>
 			<Pressable style={styles.container}>
 				<Image src={tweet.user.image} style={styles.userimage} />
@@ -58,34 +39,6 @@ const Tweet = ({ tweet }: TweetProps) => {
 				</View>
 			</Pressable>
 		</Link>
-=======
-		<View style={styles.container}>
-			<Image src={tweet.user.image} style={styles.userimage} />
-			<View style={styles.mainContainer}>
-				<View style={{ flexDirection: 'row' }}>
-					<Text style={styles.name}>{tweet.user.name}</Text>
-					<Text style={styles.username}>{tweet.user.name} ·2h</Text>
-					<Entypo
-						name='dots-three-horizontal'
-						size={16}
-						color='grey'
-						style={{ marginLeft: 'auto' }}
-					/>
-				</View>
-				<Text style={styles.content}>{tweet.content}</Text>
-
-				{tweet.image && <Image src={tweet.image} style={styles.image} />}
-
-				<View style={styles.footer}>
-					<IconButton icon='comment' text={tweet.numberOfComments} />
-					<IconButton icon='retweet' text={tweet.numberOfRetweets} />
-					<IconButton icon='heart' text={tweet.numberOfLikes} />
-					<IconButton icon='chart' text={tweet.impression || 0} />
-					<IconButton icon='share-apple'/>
-				</View>
-			</View>
-		</View>
->>>>>>> 2b0fa301eb6f0d01f4b1df124f5880925c1ff8b0
 	);
 };
 
