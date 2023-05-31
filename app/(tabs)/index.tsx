7b9126694a0e553a419864cf83901e9087893779
valon <valonmulolli@gmail.com>
2023-05-31 21:26:55 +0200

    Tweeting Page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,25 @@
-import { StyleSheet, View,FlatList} from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import Tweet from '../../components/Tweet';
 import tweets from '../../assets/data/tweets';
-
+import { Entypo } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
 	return (
-
 		<View style={styles.page}>
-			<FlatList data={tweets} renderItem={({item})=><Tweet tweet={item}/>}  />
-			{/* <Tweet tweet={tweets[0]}/> */}
+			<FlatList
+				data={tweets}
+				renderItem={({ item }) => <Tweet tweet={item} />}
+			/>
 
+			<Link href='/new-tweet' asChild>
+				<Entypo
+					name='plus'
+					size={24}
+					color='white'
+					style={styles.floatingButton}
+				/>
+			</Link>
 		</View>
 	);
 }
@@ -17,7 +27,24 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
 	page: {
 		flex: 1,
-		backgroundColor: '#fff'
-	}
+		backgroundColor: 'white',
+	},
+	floatingButton: {
+		backgroundColor: '#1C9BF0',
+		borderRadius: 25,
+		padding: 15,
+		position: 'absolute',
+		right: 15,
+		bottom: 15,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+		overflow: 'hidden',
+	},
 });
-
+// 2:53 TIME STAMP
---
+++ app/new-tweet.tsx
@@ -0,0 +1,92 @@
+import {
+	View,
+	Text,
+	StyleSheet,
+	Image,
+	TextInput,
+	Pressable,
+	SafeAreaView,
+} from 'react-native';
+import { useState } from 'react';
+import { Link, useRouter } from 'expo-router';
+
+const user = {
+	id: 'u1',
+	username: 'Harpersurfs',
+	name: 'Harper',
+	image:
+		'https://images.unsplash.com/photo-1581182830442-e8bc7babbf15?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+};
+
+export default function NewTweet() {
+	const [text, setText] = useState('');
+  const router = useRouter()
+
+	const onTweetPress = () => {
+		console.warn('Tweeted!');
+
+    setText('')
+    router.back()
+	};
+
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+			<View style={styles.container}>
+				<View style={styles.buttonContainer}>
+					<Link href='../' style={{ fontSize: 18 }}>
+						Cancel
+					</Link>
+
+					<Pressable onPress={onTweetPress} style={styles.button}>
+						<Text style={styles.buttonText}>Tweet</Text>
+					</Pressable>
+				</View>
+
+				<View style={styles.inputContainer}>
+					<Image src={user.image} style={styles.image} />
+					<TextInput
+						value={text}
+						onChangeText={setText}
+						placeholder="What's happening?"
+						multiline
+						numberOfLines={5}
+						style={{ flex: 1 }}
+					/>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 10,
+		flex: 1,
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		marginVertical: 10,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	button: {
+		backgroundColor: '#B9DCF7',
+		padding: 10,
+		paddingHorizontal: 20,
+		borderRadius: 50,
+	},
+	buttonText: {
+		color: 'white',
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+	inputContainer: {
+		flexDirection: 'row',
+	},
+	image: {
+		width: 50,
+		aspectRatio: 1,
+		borderRadius: 50,
+		marginRight: 10,
+	},
+});
