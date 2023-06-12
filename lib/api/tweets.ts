39c8300a28151f3b56422325575475e729d8ac83
valon <valonmulolli@gmail.com>
2023-06-12 16:33:53 +0200

    Fetch one Tweet

--- lib/api/tweets.ts
+++ lib/api/tweets.ts
@@ -14,3 +14,18 @@ export const ListTweets = async () => {
 	}
 	return await res.json();
 };
+
+ export const getTweet =  async (id: string) => {
+	const res = await fetch(`${API_URL}/tweet`, {
+		headers: {
+			Authorization: `Bearer ${authToken}`,
+		},
+	});
+	if (res.status === 401) {
+		throw new Error('Not Authorized. Please sign in');
+	}
+	if (res.status !== 200) {
+		throw new Error('Error fetching tweets');
+	}
+	return await res.json();
+};
